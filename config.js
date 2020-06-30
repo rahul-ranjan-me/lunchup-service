53a3d90e4cd3f25b9f8b69f6efe4159567473029
Rahul Ranjan <rahul.smile@gmail.com>
2020-06-30 19:32:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secretKey' : '12345-67890-09876-54321',
-  'mongoUrl' : 'mongodb://dbagent:dbagent@ds145293.mlab.com:45293/heroku_xbgrvtbt'
+  'mongoUrl' : 'mongodb://10.100.1.220:27017/lunchup' //'mongodb://dbagent:dbagent@ds145293.mlab.com:45293/heroku_xbgrvtbt'
 }
